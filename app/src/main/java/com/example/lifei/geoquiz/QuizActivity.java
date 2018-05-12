5cae5b609d4ca8db1f88b6418d26adaf8b8d451b
Lifei Liu <lliu.lifei@gmail.com>
2018-05-11 21:26:51 -0500

    added new features! added a new activity called cheat! so you can click cheat to see the answer of the question

--- app/src/main/java/com/example/lifei/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/lifei/geoquiz/QuizActivity.java
@@ -1,5 +1,7 @@
 package com.example.lifei.geoquiz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,13 +16,18 @@ public class QuizActivity extends AppCompatActivity {
 
     private static final String TAG = "QuizActivity" ;
     private static final String KEY_INDEX ="index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
     private Button mTrueButton;
     private Button mFalseButton;
     private TextView mQuestionTextView;
     private ImageButton mNextButton;
     private ImageButton mPrevButton;
+    private Button mCheatButton;
+
     private int mCurretIndex = 0;
+    private boolean mIsCheater;
+
     private Question[] mQuestionBank= new Question[] {
             new Question(R.string.question_australia, true),
             new Question(R.string.question_oceans, true),
@@ -91,11 +98,22 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Boolean answerIsTrue = mQuestionBank[mCurretIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this,answerIsTrue);
+                startActivityForResult(intent,REQUEST_CODE_CHEAT);
+            }
+        });
     }
 
     private void updateQuestion(){
 
         int question = mQuestionBank[mCurretIndex].getTextResId();
+        mIsCheater = false;
         mQuestionTextView.setText(question);
     }
 
@@ -104,42 +122,29 @@ public class QuizActivity extends AppCompatActivity {
         boolean answerIsTrue = mQuestionBank[mCurretIndex].isAnswerTrue();
         int messageResId = 0;
 
-        if(userPressedTrue == answerIsTrue){
-            messageResId = R.string.correct_toast;
+        if(mIsCheater){
+            messageResId = R.string.judgment_toast;
         }else{
-            messageResId = R.string.incorrect_toast;
+            if(userPressedTrue == answerIsTrue){
+                messageResId = R.string.correct_toast;
+            }else{
+                messageResId = R.string.incorrect_toast;
+            }
         }
+
         Toast.makeText(this,messageResId, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG,"onStart is called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG,"onPaused is called");
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG,"onResume is called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG,"onStop is called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy is called");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode == REQUEST_CODE_CHEAT){
+            if(data == null){
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 }
